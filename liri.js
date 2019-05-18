cfad4a01c4f80e86e02bcbffd4a22a055864737d
brianchilds-22 <brianchilds22@msn.com>
2019-05-17 18:04:15 -0600

    liri w omdb-spotify

--- liri.js
+++ liri.js
@@ -10,10 +10,18 @@ var keys = require('./keys.js');
 // Spotify API client id & Secrect
 var spotify = new Spotify(keys.spotify);
 
+// request npm
+var request = require("request");
+
+// axios
+var axios = require("axios");
+
+// Artist name function
 var getArtistNames = function(artist) {
   return artist.name;
 };
 
+// Spotify
 var getMeSpotify = function(songName) {
     if (songName === undefined) {
       songName = "What's my age again";
@@ -44,10 +52,43 @@ var getMeSpotify = function(songName) {
     );
   };
 
+// OMDB
+var movieThis = function() {
+var nodeArgs = process.argv;
+var movieName = "";
+for (var i = 3; i < nodeArgs.length; i++) {
+  if (i > 3 && i < nodeArgs.length) {
+    movieName = movieName + "+" + nodeArgs[i];
+  }
+  else {
+    movieName += nodeArgs[i];
+  }
+}
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+// console.log(response);
+axios.get(queryUrl).then(
+  function(response) {
+    // console.log(response);
+    console.log("Title: " + response.data.Title);
+    console.log("Year: " + response.data.Year)
+    console.log("IMDB Rating: " + response.data.imdbRating);
+    console.log("Metascore: " + response.data.Metascore);
+    console.log("Country: " + response.data.Country);
+    console.log("Language: " + response.data.Language);
+    console.log("Plot: " + response.data.Plot);
+    console.log("Actors: " + response.data.Actors);
+  }
+);
+};
+
+// execute with switch and case 
 var pick = function(caseData, functionData) {
     switch(caseData) {
         case 'spotify-this-song':
             getMeSpotify(functionData);
+            break;
+        case "movie-this":
+            movieThis();
             break;
         default:
         console.log('LIRI does not know that');
